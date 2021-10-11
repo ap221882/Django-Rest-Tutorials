26536eba545798f0ca4b6c7354e03690be3b8021
Ajay <ap221882@gmail.com>
2021-10-11 11:50:22 +0530

    Without json validator util

--- testm.py
+++ testm.py
@@ -1,3 +1,4 @@
+import json
 import requests
 BASE_URL = 'http://127.0.0.1:8000/'
 END_POINT = 'api/'
@@ -19,6 +20,21 @@ def get_all():
     resp = requests.get(BASE_URL+END_POINT)
     print(resp.status_code)
     print(resp.json())
+
+
+def create_resource():
+    new_emp = {
+        'eno': 600,
+        'ename': 'Shiva',
+        'esal': 7500,
+        'eaddr': 'Chhatarpur',
+    }
+    resp = requests.post(BASE_URL+END_POINT, data=json.dumps(new_emp))
+    print(resp.status_code)
+    print(resp.json())
+
+
+create_resource()
 
 
 get_resource('3')
--- withoutrestm/testapp/views.py
+++ withoutrestm/testapp/views.py
@@ -1,3 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from withoutrestm.testapp.utils import is_json
 import json
 from django.shortcuts import render
 from django.views.generic import View
@@ -23,8 +26,13 @@ class EmployeeDetailsCBV(SerializeMixin, HttpResponseMixin, View):
             return self.render_to_http_response(json_data)
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class EmployeeListCBV(HttpResponseMixin, SerializeMixin, View):
     def get(self, request, *args, **kwargs):
         qs = Employee.objects.all()
         json_data = self.serialize(qs)
+        return self.render_to_http_response(json_data)
+
+    def post(self, request, *args, **kwargs):
+        json_data = json.dumps({'msg': 'This is from post method'})
         return self.render_to_http_response(json_data)
