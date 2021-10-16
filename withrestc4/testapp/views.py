7a0fb7c5840d8b95502e95ea2a1fefd34f4d586a
Ajay <ap221882@gmail.com>
2021-10-16 23:13:14 +0530

    Custom Authentication given by REST_framework

--- withrestc4/testapp/views.py
+++ withrestc4/testapp/views.py
@@ -14,9 +14,14 @@ from .permissions import IsReadOnly, SunnyPermission
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
+# Custom
+from .authentications import CustomAuthentication
+
+
 class EmployeeCRUDCBV(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    authentication_classes = [JSONWebTokenAuthentication, ]
+    # authentication_classes = [JSONWebTokenAuthentication, ]
+    authentication_classes = [CustomAuthentication, ]
     permission_classes = [IsAuthenticated, ]
     # permission_classes = [AllowAny, ]
